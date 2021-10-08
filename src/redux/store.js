4e5b0c54d0eea86ecc11c9886ebccca3f24d0a6a
LiuZhanchen <67633541+Lance-racj@users.noreply.github.com>
2021-10-08 21:17:04 +0800

    devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,15 @@ import {createStore,combineReducers} from 'redux'
 import countReducer from './reducers/count_reducers'
 //引入为Person组件服务的reducer
 import personReducer from './reducers/person_reducer'
+//引入redux开发者工具
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+
+
 //合并reducers,combineReducers里面传入的对象值就是store里面保存的值
 const allReducers = combineReducers({
     firstCount:countReducer,
     secondPerson:personReducer
 })
 //将创建的store默认暴露出去，整个文件只有一个store
-export default createStore(allReducers);
+export default createStore(allReducers,composeWithDevTools());
